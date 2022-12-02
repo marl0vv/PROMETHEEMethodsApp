6be921d926dcd043aea1cead303375b194605e00
marl0vv <23glad02@mail.ru>
2022-12-03 01:30:57 +1000

    fixes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,7 +14,7 @@ MainWindow::MainWindow(QWidget *parent)
     QFont font("Roboto", 8);
     QApplication::setFont(font);
 
-   ui->tableWidget->setColumnWidth(0, 30);
+    ui->tableWidget->setColumnWidth(0, 30);
     //ui->tableWidget->resizeRowsToContents();
 
 }
@@ -32,47 +32,53 @@ void MainWindow::buildTable()
     ui->tableWidget->setColumnCount(m_criterias + m_defaultColumnCount);
     ui->tableWidget->setRowCount(m_actions + m_defaultRowCount);
 
-    //выделяем серым заголовок Свойств
+
     QFont font("Roboto", 8, QFont::Bold);
+
+    //выделяем серым заголовок Свойств
     for (int i = 0; i < m_criterias + m_defaultColumnCount; i++)
     {
-        ui->tableWidget->setItem(4, i, new QTableWidgetItem);
-        ui->tableWidget->item(4, i)->setBackground(QColorConstants::LightGray);
+        QTableWidgetItem* props = new QTableWidgetItem;
+        props->setBackground(QColorConstants::LightGray);
+        ui->tableWidget->setItem(4, i, props);
     }
-    ui->tableWidget->setItem(4, 1, new QTableWidgetItem("Свойства"));
-    ui->tableWidget->item(4, 1)->setBackground(QColorConstants::LightGray);
-    ui->tableWidget->item(4, 1)->setFont(font);
+    QTableWidgetItem* optionsLabel = new QTableWidgetItem("Свойства");
+    optionsLabel->setBackground(QColorConstants::LightGray);
+    optionsLabel->setFont(font);
+    ui->tableWidget->setItem(4, 1, optionsLabel);
 
     //выделяем серым заголовок Статистика
     for (int i = 0; i < m_criterias + m_defaultColumnCount; i++)
     {
-        ui->tableWidget->setItem(7, i, new QTableWidgetItem);
-        ui->tableWidget->item(7, i)->setBackground(QColorConstants::LightGray);
+        QTableWidgetItem* props = new QTableWidgetItem;
+        props->setBackground(QColorConstants::LightGray);
+        ui->tableWidget->setItem(7, i, props);
     }
-    ui->tableWidget->setItem(7, 1, new QTableWidgetItem("Статистика"));
-    ui->tableWidget->item(7, 1)->setBackground(QColorConstants::LightGray);
-    ui->tableWidget->item(7, 1)->setFont(font);
-
+    QTableWidgetItem* statLabel = new QTableWidgetItem("Статистика");
+    statLabel->setBackground(QColorConstants::LightGray);
+    statLabel->setFont(font);
+    ui->tableWidget->setItem(7, 1, statLabel);
 
     //выделяем серым заголовок Альтернативы
     for (int i = 0; i < m_criterias + m_defaultColumnCount; i++)
     {
-        ui->tableWidget->setItem(12, i, new QTableWidgetItem);
-        ui->tableWidget->item(12, i)->setBackground(QColorConstants::LightGray);
+        QTableWidgetItem* props = new QTableWidgetItem;
+        props->setBackground(QColorConstants::LightGray);
+        ui->tableWidget->setItem(12, i, props);
     }
-    ui->tableWidget->setItem(12, 1, new QTableWidgetItem("Альтернативы"));
-    ui->tableWidget->item(12, 1)->setBackground(QColorConstants::LightGray);
-    ui->tableWidget->item(12, 1)->setFont(font);
+    QTableWidgetItem* altLabel = new QTableWidgetItem("Альтернативы");
+    altLabel->setBackground(QColorConstants::LightGray);
+    altLabel->setFont(font);
+    ui->tableWidget->setItem(12, 1, altLabel);
 
     //Дальше идёт заполнение остальных столбцов и таблиц заглушками, которые пользователь будет менять на свои значения
 
-    //это очень плохо. criteriaNumber - номер критерия, который будет выводится в названии-заглушке
+    //это очень плохо
     //setItem идёт в 2+i, потому что 2 - это имя строки
-    for (int i = 0, criteriaNumber = 1; i < m_criterias; i++)
+    for (int i = 0; i < m_criterias; i++)
     {
-        QString criteriaName = "Критерий" + QString::number(criteriaNumber);
+        QString criteriaName = "Критерий" + QString::number(i+1);
         ui->tableWidget->setItem(1, 2+i, new QTableWidgetItem(criteriaName));
-        criteriaNumber++;
 
         //надо бы потом это delete, наверное
         //вообще изменение этого комбобокса должно будет влиять на строку
@@ -107,11 +113,10 @@ void MainWindow::buildTable()
     }
 
     //заполняю строки с actions
-    for (int i = 0, actionNumber = 1; i < m_actions; i++)
+    for (int i = 0; i < m_actions; i++)
     {
-        QString actionName = "Критерий" + QString::number(actionNumber);
+        QString actionName = "Критерий" + QString::number(i+1);
         ui->tableWidget->setItem(13+i, 1, new QTableWidgetItem(actionName));
-        actionNumber++;
     }
 
     //заполняем строки с actions и criterias
