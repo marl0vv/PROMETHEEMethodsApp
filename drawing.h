799a75e4c10acbec1d019bddb897aa508477326a
Awernett <99848865+Awernett@users.noreply.github.com>
2023-01-06 16:37:16 +1000

    Added Complete Rankings

--- drawing.h
+++ drawing.h
@@ -18,6 +18,14 @@ private:
     Ui::Drawing *ui;
     std::vector<Actions> m_drawingAlternatives; //все альтернативы в векторе
     int m_drawingAlternativesAmount; //кол-во альтернатив
+    int m_windowHeight = 800;
+    int m_windowWidth = 800;
+    int m_verticalIndent = 100;
+    int m_phiRectangleHeight = 600;
+    int m_phiPosLeftEdge = 100;
+    int m_phiPosRightEdge = 120;
+    int m_phiNegLeftEdge = 680;
+    int m_phiNegRightEdge = 700;
 };
 
 #endif // DRAWING_H
